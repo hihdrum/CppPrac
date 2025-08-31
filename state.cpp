0590618704fca474b6fab4bd042f2369ba398a03
makoto <88492711+hihdrum@users.noreply.github.com>
2025-09-01 03:13:31 +0900

    修正

--- state.cpp
+++ state.cpp
@@ -3,10 +3,17 @@
 #include <memory>
 
 // 前方宣言
+class Event;
 class State;
 class StateS01;
 class StateS02;
 class StateS03;
+
+enum StateType {
+  S01,
+  S02,
+  S03,
+};
 
 // グローバルな状態リポジトリ
 // std::unique_ptr を使うことでメモリ管理を自動化
@@ -16,9 +23,9 @@ std::unique_ptr<State> g_StateRepo;
 class Event {
 public:
   // dispatchメソッドで、適切な状態の処理関数を呼び出す
-  virtual void dispatch(StateS01& state) = 0;
-  virtual void dispatch(StateS02& state) = 0;
-  virtual void dispatch(StateS03& state) = 0;
+  virtual StateType dispatch(StateS01& state) = 0;
+  virtual StateType dispatch(StateS02& state) = 0;
+  virtual StateType dispatch(StateS03& state) = 0;
   virtual ~Event() = default;
 };
 
@@ -26,30 +33,30 @@ public:
 class State {
 public:
   // イベントを受け取る仮想関数
-  virtual void handleEvent(Event& event) = 0;
+  virtual StateType handleEvent(Event& event) = 0;
   virtual ~State() = default;
 };
 
 // 各状態に対応するクラス
 class StateS01 : public State {
 public:
-  void handleEvent(Event& event) override {
+  StateType handleEvent(Event& event) override {
     // Visitorパターンを利用して適切な dispatch メソッドを呼び出す
-    event.dispatch(*this);
+    return event.dispatch(*this);
   }
 };
 
 class StateS02 : public State {
 public:
-  void handleEvent(Event& event) override {
-    event.dispatch(*this);
+  StateType handleEvent(Event& event) override {
+    return event.dispatch(*this);
   }
 };
 
 class StateS03 : public State {
 public:
-  void handleEvent(Event& event) override {
-    event.dispatch(*this);
+  StateType handleEvent(Event& event) override {
+    return event.dispatch(*this);
   }
 };
 
@@ -60,19 +67,19 @@ private:
 public:
   EventMsg01(const Msg01& msg) : m_msg(msg) {}
 
-  void dispatch(StateS01& state) override {
+  StateType dispatch(StateS01& state) override {
     std::cout << "Transition from S01 to S02 via Msg01" << std::endl;
-    g_StateRepo.reset(new StateS02()); // 状態をS02に遷移
+    return S02;
   }
 
-  void dispatch(StateS02& state) override {
+  StateType dispatch(StateS02& state) override {
     std::cout << "No process for Msg01 in state S02" << std::endl;
-    // 何もしない（遷移なし）
+    return S02;
   }
 
-  void dispatch(StateS03& state) override {
+  StateType dispatch(StateS03& state) override {
     std::cout << "No process for Msg01 in state S03" << std::endl;
-    // 何もしない（遷移なし）
+    return S03;
   }
 };
 
@@ -82,17 +89,19 @@ private:
 public:
   EventMsg02(const Msg02& msg) : m_msg(msg) {}
 
-  void dispatch(StateS01& state) override {
+  StateType dispatch(StateS01& state) override {
     std::cout << "No process for Msg02 in state S01" << std::endl;
+    return S01;
   }
 
-  void dispatch(StateS02& state) override {
+  StateType dispatch(StateS02& state) override {
     std::cout << "Transition from S02 to S03 via Msg02" << std::endl;
-    g_StateRepo.reset(new StateS03());
+    return S03;
   }
 
-  void dispatch(StateS03& state) override {
+  StateType dispatch(StateS03& state) override {
     std::cout << "No process for Msg02 in state S03" << std::endl;
+    return S03;
   }
 };
 
@@ -102,17 +111,19 @@ private:
 public:
   EventMsg03(const Msg03& msg) : m_msg(msg) {}
 
-  void dispatch(StateS01& state) override {
+  StateType dispatch(StateS01& state) override {
     std::cout << "No process for Msg03 in state S01" << std::endl;
+    return S01;
   }
 
-  void dispatch(StateS02& state) override {
+  StateType dispatch(StateS02& state) override {
     std::cout << "No process for Msg03 in state S02" << std::endl;
+    return S02;
   }
 
-  void dispatch(StateS03& state) override {
+  StateType dispatch(StateS03& state) override {
     std::cout << "Transition from S03 to S01 via Msg03" << std::endl;
-    g_StateRepo.reset(new StateS01());
+    return S01;
   }
 };
 
@@ -130,30 +141,66 @@ std::unique_ptr<Event> createEvent(MsgBase* msg) {
   }
 }
 
-// イベント処理のメイン関数
-void trans(MsgBase* msg) {
-  auto event = createEvent(msg);
-  if (event) {
-    g_StateRepo->handleEvent(*event);
+class StateMachineService {
+private:
+  std::unique_ptr<State> m_currentState;
+
+  std::unique_ptr<State> createState(StateType type)
+  {
+    switch(type) {
+      case S01: return std::make_unique<StateS01>();
+      case S02: return std::make_unique<StateS02>();
+      case S03: return std::make_unique<StateS03>();
+      default: return nullptr;
+    }
   }
-}
+
+public:
+  StateMachineService() : m_currentState(std::make_unique<StateS01>()) { }
+
+  void processMessage(MsgBase* msg)
+  {
+    auto event = createEvent(msg);
+    if(event && m_currentState)
+    {
+      StateType nextState = m_currentState->handleEvent(*event);
+
+      if(nextState != S01 && nextState != S02 && nextState != S03)
+      {
+        return;
+      }
+
+      if(nextState != getCurrentStateType())
+      {
+        m_currentState = createState(nextState);
+      }
+    }
+  }
+
+  StateType getCurrentStateType() const {
+    if (dynamic_cast<StateS01*>(m_currentState.get())) return S01;
+    if (dynamic_cast<StateS02*>(m_currentState.get())) return S02;
+    if (dynamic_cast<StateS03*>(m_currentState.get())) return S03;
+
+    return S01;
+  }
+};
 
 int main(void) {
-  // 初期状態をS01に設定
-  g_StateRepo.reset(new StateS01());
 
-  // テスト用のメッセージを作成
+  StateMachineService service;
+
   Msg01 msg01 = {0, 0, D_MSG_ID_01, 0, 0, 0, 0};
   Msg02 msg02 = {0, 0, D_MSG_ID_02, 0, 0, {0, 0}};
   Msg03 msg03 = {0, 0, D_MSG_ID_03, 0, "", ""};
 
   // シーケンス
-  trans(reinterpret_cast<MsgBase*>(&msg01)); // S01 -> S02
-  trans(reinterpret_cast<MsgBase*>(&msg02)); // S02 -> S03
-  trans(reinterpret_cast<MsgBase*>(&msg03)); // S03 -> S01
+  service.processMessage(reinterpret_cast<MsgBase*>(&msg01));
+  service.processMessage(reinterpret_cast<MsgBase*>(&msg02));
+  service.processMessage(reinterpret_cast<MsgBase*>(&msg03));
 
   // 不正なシーケンスのテスト
-  trans(reinterpret_cast<MsgBase*>(&msg02)); // S01でMsg02を受信 -> No Process
+  service.processMessage(reinterpret_cast<MsgBase*>(&msg02));
 
   return 0;
 }
