961d8cbc911ca586464047205c46732f3aad415f
makoto <88492711+hihdrum@users.noreply.github.com>
2025-08-31 19:57:44 +0900

    ライブラリ役のヘッダファイルを分離した。

--- c_lib.cpp
+++ c_lib.cpp
@@ -4,55 +4,7 @@
 #include <map>
 #include <functional>
 
-#define D_MSG_ID_01 0x01
-#define D_MSG_ID_02 0x02
-#define D_MSG_ID_03 0x02
-
-/* ここから外部提供のライブラリ(変更不可) */
-extern "C" {
-struct MsgBase {
-  unsigned char from;
-  unsigned char to;
-  unsigned char msgId;
-  unsigned char no_use;
-  unsigned char data[12];
-};
-
-struct Msg01 {
-  unsigned char from;
-  unsigned char to;
-  unsigned char msgId;
-  unsigned char no_use;
-  int comData1;
-  int d2;
-  int d3;
-};
-
-struct Msg02 {
-  unsigned char from;
-  unsigned char to;
-  unsigned char msgId;
-  unsigned char no_use;
-  int comData1;
-  int d[2];
-};
-
-struct Msg03 {
-  unsigned char from;
-  unsigned char to;
-  unsigned char msgId;
-  unsigned char no_use;
-  char name[6];
-  char data[6];
-};
-
-void read_msg(int fd, void *msg) {
-  read(fd, msg, sizeof(struct MsgBase));
-}
-}
-/* ここまで変更不可 */
-
-/* ここから変更可 */
+#include "msg.h"
 
 class Msg {
 public:
---
+++ msg.h
@@ -0,0 +1,54 @@
+#include <unistd.h>
+
+/* ここから外部提供のライブラリ(変更不可) */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define D_MSG_ID_01 0x01
+#define D_MSG_ID_02 0x02
+#define D_MSG_ID_03 0x02
+
+struct MsgBase {
+  unsigned char from;
+  unsigned char to;
+  unsigned char msgId;
+  unsigned char no_use;
+  unsigned char data[12];
+};
+
+struct Msg01 {
+  unsigned char from;
+  unsigned char to;
+  unsigned char msgId;
+  unsigned char no_use;
+  int comData1;
+  int d2;
+  int d3;
+};
+
+struct Msg02 {
+  unsigned char from;
+  unsigned char to;
+  unsigned char msgId;
+  unsigned char no_use;
+  int comData1;
+  int d[2];
+};
+
+struct Msg03 {
+  unsigned char from;
+  unsigned char to;
+  unsigned char msgId;
+  unsigned char no_use;
+  char name[6];
+  char data[6];
+};
+
+void read_msg(int fd, void *msg) {
+  read(fd, msg, sizeof(struct MsgBase));
+}
+#ifdef __cplusplus
+}
+#endif
+/* ここまで変更不可 */
